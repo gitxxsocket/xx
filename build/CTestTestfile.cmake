b8e004038f5a6491b7fc36fe54c6728f724a15a9
gitxxsocket <gitxxsocket@outlook.com>
2022-01-30 21:27:04 +0900

    1

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/vxdfs/Desktop/t
-# Build directory: C:/Users/vxdfs/Desktop/t/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
